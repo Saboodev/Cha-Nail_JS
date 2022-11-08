6ab0588944c5b09324062005e87d7bc58e239912
saboodev <sabrina_desantis@hotmail.fr>
2022-11-08 16:14:14 +0100

    ajout de modal sur la galerie

--- app.js
+++ app.js
@@ -11,3 +11,22 @@
         document.body.classList.toggle("dark-mode");
     })
 })();
+
+let allImg = [...document.getElementsByClassName("picture")];
+let modalBg = document.getElementById("modal-bg");
+let modalImg = document.getElementById("modal-img");
+
+const openModal = (e) => {
+    let clickedImageSrc = e.target.src;
+    modalBg.classList.add("active");
+    modalImg.src = clickedImageSrc;
+};
+
+const closeModal = () => {
+    modalBg.classList.remove("active");
+};
+
+allImg.forEach((el) => el.addEventListener("click", openModal));
+
+modalImg.addEventListener("click", (e) => e.stopPropagation());
+modalBg.addEventListener("click", closeModal);
